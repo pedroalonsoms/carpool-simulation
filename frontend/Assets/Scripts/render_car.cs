25a2def097505d1df3e006e7439814107e34fbd3
Felipegonac0 <felipegonac0@gmail.com>
2023-08-22 19:56:58 -0600

    car scale fixed

--- frontend/Assets/Scripts/render_car.cs
+++ frontend/Assets/Scripts/render_car.cs
@@ -16,7 +16,7 @@ public class render_car : MonoBehaviour
         for (int i = 0; i < clones; i++)
         {
             total_clones[i] = Instantiate(prefabCar, new Vector3(x, y, z + (i * 80)), Quaternion.Euler(0, 0, 0));
-            total_clones[i].transform.localScale = new Vector3(10, 10, 10);
+            total_clones[i].transform.localScale = new Vector3(45, 45, 45);
         }
     }
 
